e76d84b45194062f3fcb358d54b74719dcc0f8ba
ANNA CLARA MAIA FERNANDES <1090482321016@fatec-srv.local>
2025-07-11 08:57:45 -0300

    11/07 PROVA SUBSTITUTIVA

--- karangos/src/pages/SobreProjeto.jsx
+++ karangos/src/pages/SobreProjeto.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
-import Typography from '@mui/material/Typography'
-import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import foto from '../assets/vintage-cars.png'
 
 export default function SobreProjeto() {
-    const [state, setState] = React.useState('')
+    const [likes, setLikes] = React.useState(() => {
+        
+        const storedLikes = window.localStorage.getItem('likes');
+        
+        return storedLikes ? Number(storedLikes) : 0;
+    })
     
+    React.useEffect(() => {
+        window.localStorage.setItem('likes', likes)
+    }, [likes])
+
+    const [state, setState] = React.useState('')
+
     async function loadData() {
         try {
           const response = await fetch(`${import.meta.env.VITE_API_BASE}/sobre/1`)
@@ -27,12 +44,35 @@ export default function SobreProjeto() {
             Sobre o Projeto Karangos
         </Typography>
 
-        <Box>
-         <Typography variant='body1'>
-            {state}
-         </Typography>
-        </Box>
+        <Card sx={{ maxWidth: 400 }}>
+            
+        <CardMedia
+            sx={{ height: 300 }}
+            image={foto}
+            title="vintage-cars.png"
+        />
+
+        <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                Sobre o Projeto Karangos
+            </Typography>
+            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                {state}
+            </Typography>
+        </CardContent>
+
+        <CardActions>
+            <Button 
+            variant="contained"
+            color="secondary"
+            startIcon={<FavoriteIcon />}
+            onClick={() => setLikes(Number(likes) + 1)}
+            >
+            Curtir ({likes})
+            </Button>
+        </CardActions>
         
+        </Card>
         </>
-    )
+    );
 }
